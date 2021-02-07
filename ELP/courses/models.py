01e074184181640cbe637ae59330afba5e282968
PyScolok <doc.dr2011@mail.ru>
2021-02-07 20:41:43 +0300

    Created and registrated data models.

--- ELP/courses/models.py
+++ ELP/courses/models.py
@@ -1,3 +1,90 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+from .fields import OrderField
+
+
+User = get_user_model()
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    def __str__(self):
+        return f'{self.order}. {self.title}'
+
+    class Meta:
+        ordering = ['order']
+
+
+class Content(models.Model):
+    types = ('text', 'video', 'image', 'file', )
+
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='content')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in': types})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=[module])
+
+    class Meta:
+        ordering = ['order']
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_related')
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.ImageField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
